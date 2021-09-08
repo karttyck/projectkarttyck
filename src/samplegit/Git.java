a966f02d6f0e20ef3b8b7cd86061e9560cf96348
karttyck <karthicksachin987@gmail.com>
2021-09-07 20:48:25 -0700

    username

--- src/samplegit/Git.java
+++ src/samplegit/Git.java
@@ -1,8 +1,11 @@
 package samplegit;
 
 public class Git {
-	public void karty() {
+	public static void karty() {
 		System.out.println("username");
+	}
+	public static void main(String[] args) {
+		karty();
 	}
 
 	
